d9b726425098809dc1a0594359c535798e03e4b1
Emmanuel Muchiri <Emmanuel.Muchiri@outlook.com>
2019-06-19 00:11:27 +0300

    did more refactoring to improve on the toggle effect

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
   $("#car").click(function() {
     $(".platinum").fadeIn();
+    $(".premium").hide();
+    $(".gold").hide();
+    $(".deluxe").hide();
+  });
+  $("#pick-up").click(function() {
+    $(".platinum").hide();
+    $(".premium").fadeIn();
+    $(".gold").hide();
+    $(".deluxe").hide();
+  });
+  $("#minivan").click(function() {
+    $(".platinum").hide();
+    $(".premium").hide();
+    $(".gold").fadeIn();
+    $(".deluxe").hide();
+  });
+  $("#van").click(function() {
+    $(".platinum").hide();
+    $(".premium").hide();
+    $(".gold").hide();
+    $(".deluxe").fadeIn();
   });
 });
