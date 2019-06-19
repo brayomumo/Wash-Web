7de0ed4194ed1917e78f6b4c5fc433254f6f7f91
brian <brayomumo>
2019-06-19 09:10:44 +0300

    Change Effects for the divs

--- js/script.js
+++ js/script.js
@@ -1,31 +1,26 @@
 $(document).ready(function() {
-  var appenderClass = $(".appended").val
-  for (var i = 0; i<appenderClass.length; i++){
-      $('.cars').click(function(){
-          $('.appendedDiv').append()
-      })
-  }
-
-//   $("#car").click(function() {
-//     $(".platinum").slideToggle("slow");
-//     $(".premium").hide();
-//     $(".gold").hide();
-//     $(".deluxe").hide();
-//   });
-//   $("#pick-up").click(function() {
-//     $(".platinum").slideUp();
-//     $(".premi.rowum").fadeIn("slow");
-//     $(".gold").hide();
-//     $(".deluxe").hide();
-//   });
-//   $("#minivan").click(function() {
-//     $(".platinum").hide();
-//     $(".premium").hide();
-//     $(".gold").slideToggle("slow");
-//     $(".deluxe").hide();
-//   });
-//   $("#van").click(function() {
-//     $(".platinum").hide();
-//     $(".premium").hide();
-//   })
-})
+    $("#car").click(function() {
+      $(".platinum").show();
+      $(".premium").hide();
+      $(".gold").hide();
+      $(".deluxe").hide();
+    });
+    $("#pick-up").click(function() {
+      $(".platinum").hide();
+      $(".premium").fadeIn("slow");
+      $(".gold").hide();
+      $(".deluxe").hide();
+    });
+    $("#minivan").click(function() {
+      $(".platinum").show();
+      $(".premium").hide();
+      $(".gold").fadeToggle("slow");
+      $(".deluxe").hide();
+    });
+    $("#van").click(function() {
+      $(".platinum").show();
+      $(".premium").hide();
+      $(".gold").hide();
+      $(".deluxe").fadeToggle("slow");
+    });
+  })
