93e2f4fe885b68668facc1ecf6e34e690005a4a5
brian <brayomumo>
2019-06-19 07:16:37 +0300

    Add toggle function

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('#clicked1').click(function () {
+        $("#car1").show();
+    })
+    $('#clicked2').click(function () {
+        $("#car2").show();
+    })
+    $('#clicked3').click(function () {
+        $("#car3").show();
+    })
+    $('#clicked4').click(function () {
+        $("#car4").show();
+    })
+})
