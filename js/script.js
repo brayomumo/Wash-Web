46421b5cd40394b8651219ce3220d3e24f1e94ce
brian <brayomumo>
2019-06-19 07:36:52 +0300

    Add toggle function

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-$(document).ready(function () {
-    $('#clicked1').click(function () {
-        $("#car1").show();
-    })
-    $('#clicked2').click(function () {
-        $("#car2").show();
-    })
-    $('#clicked3').click(function () {
-        $("#car3").show();
-    })
-    $('#clicked4').click(function () {
-        $("#car4").show();
-    })
-})
-=======
 $(document).ready(function() {
   $("#car").click(function() {
     $(".platinum").slideToggle("slow");
@@ -40,4 +24,3 @@ $(document).ready(function() {
     $(".deluxe").fadeToggle("slow");
   });
 });
->>>>>>> 49c0c1e4a1145b3b56539b4bf7b95df936892042
